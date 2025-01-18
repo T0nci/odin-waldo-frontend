80b9d6cbf5e703e413c83c6cde78a4c2e9d09f7c
T0nci <138928016+T0nci@users.noreply.github.com>
2025-01-18 16:59:53 +0100

    Add functionality to Map for playing the game
    Wrote tests for Map as well.

--- src/Components/partials/Map/Map.jsx
+++ src/Components/partials/Map/Map.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 import styles from "./Map.module.css";
 import Dropdown from "../Dropdown/Dropdown";
 
-const Map = () => {
+const Map = ({ url, characters, guessCharacter, setGuessResult }) => {
   const [dropdown, setDropdown] = useState({
     active: false,
     coordinates: [],
+    left: false,
   });
 
+  const turnOffDropdown = () => {
+    setDropdown({ active: false, coordinates: [], left: false });
+  };
   const handleClick = (e) => {
-    if (dropdown.active) setDropdown({ active: false, coordinates: [] });
+    if (dropdown.active) turnOffDropdown();
     else {
-      console.log(`Offset: ${e.nativeEvent.offsetX}, ${e.nativeEvent.offsetY}`);
+      let left = false;
+      if (e.clientX * 2 > document.body.clientWidth) left = true;
 
       setDropdown({
         active: true,
         // these coordinates are relative to the element being clicked on, and since we are loading the full image these are accurate
         coordinates: [e.nativeEvent.offsetX, e.nativeEvent.offsetY],
+        left,
       });
     }
   };
@@ -24,22 +31,38 @@ const Map = () => {
   return (
     <>
       <div className={styles.map}>
-        <img
-          src="https://res.cloudinary.com/dyi9mrxgm/image/upload/v1736456545/terraria-map-1_fdytsc.jpg"
-          alt="..."
-          onClick={handleClick}
-        />
+        <img src={url} alt="map" onClick={handleClick} />
         {dropdown.active && (
           <Dropdown
             style={{
               top: dropdown.coordinates[1],
               left: dropdown.coordinates[0],
+              transform: dropdown.left ? "translateX(-100%)" : "",
             }}
+            coordinates={dropdown.coordinates}
+            characters={characters}
+            guessCharacter={guessCharacter}
+            turnOffDropdown={turnOffDropdown}
+            setGuessResult={setGuessResult}
           />
         )}
       </div>
     </>
   );
+};
+
+Map.propTypes = {
+  url: PropTypes.string.isRequired,
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessed: PropTypes.bool,
+      id: PropTypes.number,
+      name: PropTypes.string,
+      url: PropTypes.string,
+    }),
+  ).isRequired,
+  guessCharacter: PropTypes.func.isRequired,
+  setGuessResult: PropTypes.func.isRequired,
 };
 
 export default Map;
---
+++ src/Components/partials/Map/Map.test.jsx
@@ -0,0 +1,169 @@
+/* eslint-disable no-undef */
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Map from "./Map";
+
+global.fetch = vi.fn();
+
+const hoisted = vi.hoisted(() => ({
+  navigate: vi.fn(),
+}));
+vi.mock(import("react-router"), async (importOriginal) => {
+  const mod = await importOriginal();
+
+  const useNavigate = () => hoisted.navigate;
+
+  return {
+    ...mod,
+    useNavigate,
+  };
+});
+
+const characters = [
+  {
+    guessed: false,
+    id: 1,
+    name: "Test Character 1",
+    url: "Test URL 3",
+  },
+  {
+    guessed: false,
+    id: 2,
+    name: "Test Character 2",
+    url: "Test URL 4",
+  },
+];
+
+describe("Map Component", () => {
+  it("renders", () => {
+    const { container } = render(
+      <Map
+        url={"Test URL 6"}
+        characters={characters}
+        guessCharacter={() => {}}
+        setGuessResult={() => {}}
+      />,
+    );
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <div
+          class="_map_d20351"
+        >
+          <img
+            alt="map"
+            src="Test URL 6"
+          />
+        </div>
+      </div>
+    `);
+  });
+
+  it("renders with dropdown", async () => {
+    const { container } = render(
+      <Map
+        url={"Test URL 6"}
+        characters={characters}
+        guessCharacter={() => {}}
+        setGuessResult={() => {}}
+      />,
+    );
+    const user = userEvent.setup();
+
+    await user.click(screen.getByRole("img"));
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <div
+          class="_map_d20351"
+        >
+          <img
+            alt="map"
+            src="Test URL 6"
+          />
+          <ul
+            class="_dropdown_943d41"
+            style="top: 0px; left: 0px;"
+          >
+            <li>
+              <button
+                class="_character_943d41"
+              >
+                <img
+                  alt=""
+                  class="_image_943d41"
+                  src="Test URL 3"
+                />
+                <p
+                  class="_name_943d41"
+                >
+                  Test Character 1
+                </p>
+              </button>
+            </li>
+            <li>
+              <button
+                class="_character_943d41"
+              >
+                <img
+                  alt=""
+                  class="_image_943d41"
+                  src="Test URL 4"
+                />
+                <p
+                  class="_name_943d41"
+                >
+                  Test Character 2
+                </p>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    `);
+  });
+
+  it("calls functions with a correct guess", async () => {
+    const guessCharacter = vi.fn();
+    const setGuessResult = vi.fn();
+    const json = () => Promise.resolve({ result: "Correct guess" });
+    global.fetch.mockResolvedValueOnce({ json });
+
+    render(
+      <Map
+        url={"Test URL 6"}
+        characters={characters}
+        guessCharacter={guessCharacter}
+        setGuessResult={setGuessResult}
+      />,
+    );
+    const user = userEvent.setup();
+
+    await user.click(screen.getByRole("img"));
+    await user.click(screen.getAllByRole("button")[0]);
+
+    expect(guessCharacter).toHaveBeenCalledWith(1);
+    expect(setGuessResult).toHaveBeenCalledWith("Correct guess");
+  });
+
+  it("redirects to /name when game is over", async () => {
+    const json = () => Promise.resolve({ result: "Game over" });
+    global.fetch.mockResolvedValueOnce({ json });
+
+    render(
+      <Map
+        url={"Test URL 6"}
+        characters={characters}
+        guessCharacter={() => {}}
+        setGuessResult={() => {}}
+      />,
+    );
+    const user = userEvent.setup();
+
+    await user.click(screen.getByRole("img"));
+    await user.click(screen.getAllByRole("button")[0]);
+
+    expect(hoisted.navigate).toHaveBeenCalledWith("/name");
+  });
+});
